8076b0dba9242af9b3e193bfecac8036355d60ee
Jabessa Taye <jabesa4@gmail.com>
2022-10-19 03:46:39 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,20 @@
-#include "main.h"
-
+#include "main.h"
 
 /**
- * main -  prints _putchar, followed by a new line
- * Return: always 0
- */
+* main - prints _putchar, followed by a new line
+* Return: Always 0
+*/
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
 
 	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+int _putchar(char);
 void print_alphabet(void);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -13,7 +14,7 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-int _putchar(char);
+
 
 #endif
 
