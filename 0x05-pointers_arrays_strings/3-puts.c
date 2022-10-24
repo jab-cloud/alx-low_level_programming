8962788f30b62acd8e682b4acf07dd3eb3cba439
Jabessa Taye <jabesa4@gmail.com>
2022-10-24 13:14:58 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line
+ * @str: string
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}
