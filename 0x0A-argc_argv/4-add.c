7b86567e6860da15c4bddb111865812ed2ae1cde
Jabessa Taye <jabesa4@gmail.com>
2022-11-04 12:22:20 -0700

    task4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,5 @@
-#include "stdio.h"
-#include "stdlib.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - function prints its name
@@ -9,21 +9,21 @@
  */
 int main(int argc, char *argv[])
 {
-	int result = 0, num, i;
+	int result = 0, num, i, j, k;
 
-	if (argc == 1)
-		printf("0\n");
-
-	for (i = 0; i < argc; i++)
+	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
+			if (argv[i][j] > '9' || argv[i][j] < '0')
+			{
 			printf("%s\n", "Error");
 			return (1);
+			}
 		}
 	}
 
-	for (k = 0; k < argc; k++)
+	for (k = 1; k < argc; k++)
 	{
 		num = atoi(argv[k]);
 		result += num;
