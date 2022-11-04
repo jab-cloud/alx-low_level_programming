9876058d34a5c02acf55ead7285214a7e5f8a1b8
Jabessa Taye <jabesa4@gmail.com>
2022-11-04 11:10:31 -0700

    task 1 arg

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio>
+
+/**
+ * main - program that prints the number of arguments
+ * @argc: argc parameter
+ * @argv: an array of a command listed
+ * Return: 0 for success
+ */
+int main(int argc, char *argv[]  __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
