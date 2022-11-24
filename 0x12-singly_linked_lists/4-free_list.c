51e604ae92511e4e8aa3b5c708432b14b8f25816
jabessa taye <jabesa4@gmail.com>
2022-11-24 11:58:13 -0800

    task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list
+ * @head: head of the linked list.
+ * Return: no return.
+ */
+
+void free_list(list_t *head)
+{
+	list_t *current;
+
+	while (head)
+	{
+		current = head;
+		head = head->next;
+		free(current->str);
+		free(current);
+	}
+}
+
