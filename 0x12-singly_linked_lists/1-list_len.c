442b310c770e5af841a139daf92e2462abe07c17
jabessa taye <jabesa4@gmail.com>
2022-11-24 11:33:47 -0800

    task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * list_len - returns then number of elements in a list.
+ * @h: singly linked list.
+ * Return: number of elements in the list.
+ */
+
+size_t list_len(const list_t *h)
+{
+	int nodes = 0;
+
+	while (h)
+	{
+		nodes += 1
+		h = h->next;
+	}
+	return (nodes);
+}
