47b0cbb1b06725e28ba43d1609ced447ed3a4696
jabessa taye <jabesa4@gmail.com>
2023-01-20 13:05:03 -0800

    task 4

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp -q "https://github.com/jab-cloud/low_level_programming/raw/master/0x18-dynamic_libraries/libmmw.so"
-export LD_PRELOAD="/tmp/libmmw.so"
+wget -0 /tmp/win.so https://github.com/jab-cloud/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/win.so
+export LD_PRELOAD=/tmp/win.so
